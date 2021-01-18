fa4c6d3e5d1f550d5561132b1a25f98de04d3d0f
Lee JaeHyun <myleje@gmail.com>
2021-01-18 15:32:17 +0900

    .

---
+++ Assets/02.Scripts/MoveCtrl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCtrl : MonoBehaviour
+{
+    public Transform camTr; //메인카메라의 Transform 컴포넌트를 저장할 변수
+    public float moveSpeed = 0.8f;
+
+    private Transform tr;   //자신의 Transform 컴포넌트를 저장할 변수
+    private CharacterController cc; //이동처리를 위한 컴포너트
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
