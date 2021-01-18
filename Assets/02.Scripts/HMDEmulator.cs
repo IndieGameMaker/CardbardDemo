8ee1ef501894cb13ec50839799c74c88739af8be
Lee JaeHyun <myleje@gmail.com>
2021-01-18 14:37:45 +0900

    .

--- Assets/02.Scripts/HMDEmulator.cs
+++ Assets/02.Scripts/HMDEmulator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class HMDEmulator : MonoBehaviour
 {
+    public float yawSpeed = 3.0f;   //Y축 회전 속도
+    public float pitchSpeed = 3.0f; //X축 회전 속도
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +14,9 @@ public class HMDEmulator : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() //60fps  1/60 sec 발생 빈도 불규칙
     {
         
     }
+
 }
