28c48d34d9d2d89121bf606a00ad7c5abdca6388
Lee JaeHyun <myleje@gmail.com>
2021-01-18 14:50:52 +0900

    .

--- Assets/02.Scripts/HMDEmulator.cs
+++ Assets/02.Scripts/HMDEmulator.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class HMDEmulator : MonoBehaviour
 {
+    private Transform camTr;
+
     public float yawSpeed = 3.0f;   //Y축 회전 속도
     public float pitchSpeed = 3.0f; //X축 회전 속도
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        camTr = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update() //60fps  1/60 sec 발생 빈도 불규칙
     {
-        
+        if (Input.GetKey(KeyCode.LeftAlt))  //GetKeyDown, GetKeyUp
+        {
+            Debug.Log("LeftAlt pressed !!!");
+        }
     }
 
 }
