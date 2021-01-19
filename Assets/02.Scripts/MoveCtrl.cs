3c936c132a65e7054a083528051a14fe3ce57888
Lee JaeHyun <myleje@gmail.com>
2021-01-19 13:36:50 +0900

    .

--- Assets/02.Scripts/MoveCtrl.cs
+++ Assets/02.Scripts/MoveCtrl.cs
@@ -10,6 +10,8 @@ public class MoveCtrl : MonoBehaviour
     private Transform tr;   //자신의 Transform 컴포넌트를 저장할 변수
     private CharacterController cc; //이동처리를 위한 컴포너트
 
+    public bool isStopped = false;
+
     void Start()
     {
         tr = GetComponent<Transform>();
@@ -19,6 +21,8 @@ public class MoveCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isStopped == true) return;
+
         Vector3 dir = camTr.forward;
 
         cc.SimpleMove(dir * moveSpeed);
