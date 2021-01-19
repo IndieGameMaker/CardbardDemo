33275bbd3c793d52c2b64a63db9b5db04a17af6a
Lee JaeHyun <myleje@gmail.com>
2021-01-19 15:38:09 +0900

    .

---
+++ Assets/02.Scripts/UI_Button.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Button : MonoBehaviour
+{
+    void OnGazeEnter()
+    {
+        Debug.Log("Enter !!!");
+    }
+
+    void OnGazeExit()
+    {
+        Debug.Log("Exit !!!");
+    }
+}
