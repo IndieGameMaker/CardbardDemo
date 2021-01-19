edff6d3f74696afb33672f7aa39da461be9e26cf
Lee JaeHyun <myleje@gmail.com>
2021-01-19 14:26:09 +0900

    .

--- Assets/02.Scripts/CastManager.cs
+++ Assets/02.Scripts/CastManager.cs
@@ -26,7 +26,6 @@ public class CastManager : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, distance, 1<<8)) //1<<8 = 2^8 = 256
         {
-            Debug.Log("Hit = " + hit.transform.name);
             MoveCtrl.isStopped = true;
         }
         else
