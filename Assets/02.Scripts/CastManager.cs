15248f2fff84e1559bb3b981111cadecf238d2f9
Lee JaeHyun <myleje@gmail.com>
2021-01-19 13:51:26 +0900

    .

---
+++ Assets/02.Scripts/CastManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastManager : MonoBehaviour
+{
+    private Ray ray;
+    private RaycastHit hit;
+
+    private Transform camTr;
+
+    [Range(5.0f, 20.0f)]
+    public float distance = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
